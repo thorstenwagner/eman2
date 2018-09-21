990a692214a37a8f35c381acf9ab1a180bdf9c6b
shadow_walker <shadowwalkersb@gmail.com>
2018-09-21 00:43:15 -0500

    from PyQt4.QtCore import Qt

--- programs/eman2.py
+++ programs/eman2.py
@@ -61,7 +61,6 @@ helpstring+="\n\nYou are currently running %s"%(FULLVERSIONSTRING)
 try:
 	if os.getenv("DISPLAY")==None : raise Exception
 	from PyQt4 import QtCore, QtGui
-	from PyQt4.QtCore import Qt
 except:
 	print(helpstring)
 	eval(input("Please press <enter> to exit"))
