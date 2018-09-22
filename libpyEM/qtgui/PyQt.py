faec56c61dac08c68fd4ce561cf44d11d8e55371
shadow_walker <shadowwalkersb@gmail.com>
2018-09-21 23:25:25 -0500

    PyQt

--- libpyEM/qtgui/PyQt.py
+++ libpyEM/qtgui/PyQt.py
@@ -2,18 +2,10 @@ from __future__ import print_function
 from __future__ import division
 
 
+from PyQt4.QtCore import *
+from PyQt4.QtGui import *
+from PyQt4.QtOpenGL import QGLWidget
 from PyQt4 import QtCore
 from PyQt4 import QtGui
 from PyQt4 import QtOpenGL
-from PyQt4.QtCore import QEvent
-from PyQt4.QtCore import QTimer
-from PyQt4.QtCore import Qt
-from PyQt4.QtGui import QAbstractButton
-from PyQt4.QtGui import QAction
-from PyQt4.QtGui import QColor
-from PyQt4.QtGui import QImage
-from PyQt4.QtGui import QListWidgetItem
-from PyQt4.QtGui import QProgressDialog
-from PyQt4.QtGui import QTreeWidgetItem
-from PyQt4.QtOpenGL import QGLWidget
 from PyQt4 import QtWebKit
