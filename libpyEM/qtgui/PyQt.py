528709b9e06bcc9895be1e2c061c1e5c62713b9d
shadow_walker <shadowwalkersb@gmail.com>
2018-09-21 23:38:51 -0500

    PyQt: remove QtCore, QtGui

--- libpyEM/qtgui/PyQt.py
+++ libpyEM/qtgui/PyQt.py
@@ -5,7 +5,5 @@ from __future__ import division
 from PyQt4.QtCore import *
 from PyQt4.QtGui import *
 from PyQt4.QtOpenGL import QGLWidget
-from PyQt4 import QtCore
-from PyQt4 import QtGui
 from PyQt4 import QtOpenGL
 from PyQt4 import QtWebKit
